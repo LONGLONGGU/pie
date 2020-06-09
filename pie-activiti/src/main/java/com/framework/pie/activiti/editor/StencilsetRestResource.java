16727013fee0c77113a56b7f4c4bda34e4fb6d4e
longlong <Admin@1234>
2020-06-09 15:16:04 +0800

    流程设计器

---
+++ pie-activiti/src/main/java/com/framework/pie/activiti/editor/StencilsetRestResource.java
@@ -0,0 +1,25 @@
+package com.framework.pie.activiti.editor;
+
+import org.activiti.engine.ActivitiException;
+import org.apache.commons.io.IOUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.InputStream;
+
+@RestController
+public class StencilsetRestResource {
+
+    @RequestMapping(value="/editor/stencilset", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
+    public @ResponseBody
+    String getStencilset() {
+        InputStream stencilsetStream = this.getClass().getClassLoader().getResourceAsStream("stencilset.json");
+        try {
+            return IOUtils.toString(stencilsetStream, "utf-8");
+        } catch (Exception e) {
+            throw new ActivitiException("Error while loading stencil set", e);
+        }
+    }
+}
