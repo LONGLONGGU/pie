d86aace63da1fd50c3bd51e3c8e500344aeaa970
longlong <Admin@1234>
2020-09-14 10:53:35 +0800

    mybaits 更换 为 mybatis-plus

--- pie-admin/src/main/java/com/framework/pie/admin/config/MybatisConfig.java
+++ pie-admin/src/main/java/com/framework/pie/admin/config/MybatisConfig.java
@@ -1,8 +1,10 @@
 package com.framework.pie.admin.config;
 
+
+
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
