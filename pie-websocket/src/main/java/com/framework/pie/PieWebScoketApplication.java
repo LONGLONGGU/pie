c6b8fc50bf86573cc5481086f54cb5f0e86968ca
longlong <Admin@1234>
2020-09-16 10:42:03 +0800

    webscoket springboot 版本设计 cloud版本升级

---
+++ pie-websocket/src/main/java/com/framework/pie/PieWebScoketApplication.java
@@ -0,0 +1,13 @@
+package com.framework.pie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieWebScoketApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PieWebScoketApplication.class,args);
+    }
+
+
+}
