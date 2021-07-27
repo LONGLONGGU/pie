c56666373050fb14dd7a2ebe792872f0c6a82796
longlong <409070933@qq.com>
2021-07-27 10:00:53 +0800

    dept

--- pie-admin/src/main/java/com/framework/pie/admin/controller/SysDeptController.java
+++ pie-admin/src/main/java/com/framework/pie/admin/controller/SysDeptController.java
@@ -4,13 +4,10 @@ import com.framework.pie.admin.model.SysDept;
 import com.framework.pie.admin.service.SysDeptService;
 import com.framework.pie.admin.util.syslog.Log;
 import com.framework.pie.http.HttpResult;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +41,13 @@ public class SysDeptController {
 	@GetMapping(value="/findTree")
 	public HttpResult findTree() {
 		return HttpResult.ok(sysDeptService.findTree());
+	}
+
+	@ApiOperation("异步加载部门tree")
+	@GetMapping(value="/findTree/{parentId}")
+	public HttpResult asyncFindTree(@PathVariable("parentId") Long parentId) {
+
+		return HttpResult.ok(sysDeptService.findTree(parentId));
 	}
 
 
